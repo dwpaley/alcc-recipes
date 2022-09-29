437f4c256918a28d26da8f75b5b4be0eadcc8c86
Felix Wittwer <40206209+Trzs@users.noreply.github.com>
2022-09-29 09:40:00 +0200

    Update setup_crusher.sh

--- cctbx/setup_crusher.sh
+++ cctbx/setup_crusher.sh
@@ -3,6 +3,10 @@
 set -e
 
 export ALCC_CCTBX_ROOT=$(readlink -f $(dirname ${BASH_SOURCE[0]}))
+export OMP_PLACES=threads
+export OMP_PROC_BIND=spread
+export KOKKOS_DEVICES="OpenMP;HIP"
+export KOKKOS_ARCH="VEGA90A"
 
 source ${ALCC_CCTBX_ROOT}/utilities.sh
 source ${ALCC_CCTBX_ROOT}/opt/util/fix_lib_nersc.sh
@@ -35,6 +39,10 @@ source ${ALCC_CCTBX_ROOT}/utilities.sh
 source ${ALCC_CCTBX_ROOT}/opt/site/olcf_crusher.sh
 load-sysenv
 activate
+export OMP_PLACES=threads
+export OMP_PROC_BIND=spread
+export KOKKOS_DEVICES="OpenMP;HIP"
+export KOKKOS_ARCH="VEGA90A"
 export SIT_DATA=\${OVERWRITE_SIT_DATA:-\$NERSC_SIT_DAT}:\$SIT_DATA
 export SIT_PSDM_DATA=\${OVERWRITE_SIT_PSDM_DATA:-\$NERSC_SIT_PSDM_DATA}
 EOF
