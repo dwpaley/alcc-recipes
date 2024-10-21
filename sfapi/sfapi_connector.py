b03418a060773ad15fb421ba3557cf0d79af05f3
Johannes Blaschke <jpblaschke@lbl.gov>
2024-10-21 14:54:48 -0700

    start working on an SFAPI context manager that is compatible with cctbx

--- sfapi/sfapi_connector.py
+++ sfapi/sfapi_connector.py
@@ -3,9 +3,12 @@ from sfapi_client.compute import Machine
 
 from pathlib import Path
 
+import os
+import io
 import json
 
 
+# TODO: replace with simple singleton
 def load_key():
     key_store = Path("~/.superfacility/").expanduser().resolve()
 
@@ -31,3 +34,80 @@ def load_key():
             f.write(f"{client_id}\n" + "\n".join(sfapi_key_file))
 
     return client_id, sfapi_key
+
+
+class SFAPIFile:
+    def __init__(self, name):
+        self.name = name
+        self.buffer = io.StringIO()
+
+    def back_to_start(self):
+        self.buffer.seek(0)
+
+    def __getattr__(self, name):
+        return getattr(self.buffer, name)
+
+
+class OpenSFAPI:
+    def __init__(self, file_name):
+        self.lines_buffer = SFAPIFile(file_name)
+
+    def __enter__(self):
+        return self.lines_buffer
+
+    def __exit__(self, type, value, traceback):
+        self.lines_buffer.back_to_start()
+        print(self.lines_buffer.readlines())
+
+
+class OsBackend:
+    @staticmethod
+    def open(
+        file,
+        mode='r', buffering=-1, encoding=None, errors=None, newline=None,
+        closefd=True, opener=None
+    ):
+        return open(
+            file, mode, buffering, encoding, errors, newline, closefd, opener
+        )
+
+    @staticmethod
+    def mkdir(path, mode=0o777, *, dir_fd=None): 
+        return os.mkdir(path, mode, dir_fd)
+
+    @staticmethod
+    def stat(fd):
+        return os.stat(fd)
+
+    @staticmethod
+    def chmod(path, mode, *, dir_fd=None, follow_symlinks=True):
+        os.chmod(path, mode, dir_fd, follow_symlinks)
+
+
+class OsSFAPI:
+    @staticmethod
+    def open(
+        file,
+        mode='r', buffering=-1, encoding=None, errors=None, newline=None,
+        closefd=True, opener=None, key=None
+    ):
+        return OpenSFAPI(file)
+
+    @staticmethod
+    def mkdir(path, mode=0o777, *, dir_fd=None): 
+        pass
+
+    @staticmethod
+    def stat(fd):
+        pass
+
+    @staticmethod
+    def chmod(path, mode, *, dir_fd=None, follow_symlinks=True):
+        pass
+
+
+class OsWrapper:
+
+    def __init__(self, backend):
+        pass
+
